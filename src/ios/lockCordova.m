343da3892734ddf0cc39c5b83d007d16af99eac4
Pablo Terradillos <tehsis@gmail.com>
2016-12-28 10:48:09 -0300

    Added a0 config

--- src/ios/lockCordova.m
+++ src/ios/lockCordova.m
@@ -1,34 +1,23 @@
 /********* lockCordova.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
-#import <Lock/Lock.h>
 
 @interface lockCordova : CDVPlugin {
   // Member variables go here.
 }
 
-- (void)show:(CDVInvokedUrlCommand*)command;
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation lockCordova
 
-- (void)show:(CDVInvokedUrlCommand*)command
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
-    A0Lock *lock = [A0Lock sharedLock];
-    A0LockViewController *controller = [lock newLockViewController];
-    controller.onAuthenticationBlock = ^(A0UserProfile *profile, A0Token *token) {
-        // Do something with token & profile. e.g.: save them.
-        // Lock will not save the Token and the profile for you.
-        // And dismiss the UIViewController.
-        [self dismissViewControllerAnimated:YES completion:nil];
-    };
-    [lock presentLockController:controller fromController:self];
 
     if (echo != nil && [echo length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-        NSLog(echo);
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
--- www/lockCordova.js
+++ www/lockCordova.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.show = function(arg0, success, error) {
-    exec(success, error, "lockCordova", "show", [arg0]);
+exports.coolMethod = function(arg0, success, error) {
+    exec(success, error, "lockCordova", "coolMethod", [arg0]);
 };
