44dd45b1ee945ea8a4873f88ff8712ea2a66341e
Pablo Terradillos <tehsis@gmail.com>
2016-12-28 10:28:05 -0300

    Added a0 config

--- src/ios/lockCordova.m
+++ src/ios/lockCordova.m
@@ -16,6 +16,15 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
+    A0Lock *lock = [A0Lock sharedLock];
+    A0LockViewController *controller = [lock newLockViewController];
+    controller.onAuthenticationBlock = ^(A0UserProfile *profile, A0Token *token) {
+        // Do something with token & profile. e.g.: save them.
+        // Lock will not save the Token and the profile for you.
+        // And dismiss the UIViewController.
+        [self dismissViewControllerAnimated:YES completion:nil];
+    };
+    [lock presentLockController:controller fromController:self];
 
     if (echo != nil && [echo length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
