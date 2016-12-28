d6e42c2ac326827d0b1c50039c261f33510ce2dc
Pablo Terradillos <tehsis@gmail.com>
2016-12-28 11:39:19 -0300

    Initial commit

--- src/ios/lockCordova.m
+++ src/ios/lockCordova.m
@@ -1,6 +1,7 @@
 /********* lockCordova.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
+#import <Lock/Lock.h>
 
 @interface lockCordova : CDVPlugin {
   // Member variables go here.
@@ -15,6 +16,16 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
+
+    A0Lock *lock = [A0Lock sharedLock];
+A0LockViewController *controller = [lock newLockViewController];
+controller.onAuthenticationBlock = ^(A0UserProfile *profile, A0Token *token) {
+    // Do something with token & profile. e.g.: save them.
+    // Lock will not save the Token and the profile for you.
+    // And dismiss the UIViewController.
+    [self dismissViewControllerAnimated:YES completion:nil];
+};
+[lock presentLockController:controller fromController:self];
 
     if (echo != nil && [echo length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
